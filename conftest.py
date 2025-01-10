99b568943cd71220b977efe1e627be5eda10cf1d
Diana <obriendiana235@gmail.com>
2025-01-10 20:22:16 +0500

    config_json

--- conftest.py
+++ conftest.py
@@ -1,17 +1,53 @@
 import pytest
+import json
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+
+class ConfigReader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.config = self._load_config()
+
+    def _load_config(self):
+        try:
+            with open(self.file_path, "r") as file:
+                return json.load(file)
+        except FileNotFoundError:
+            raise FileNotFoundError(f'Файл конфигурации {self.file_path} не найден.')
+        except json.JSONDecodeError:
+            raise ValueError(f'Ошибка в декодировании JSON-файла {self.file_path}.')
+
+    def get(self, key, default=None):
+        return self.config.get(key, default)
+
+    def update(self, key, value):
+        self.config[key] = value
+        self.save()
+
+    def save(self):
+        try:
+            with open(self.file_path, "w") as file:
+                json.dump(self.config, file, indent=4)
+        except Exception as e:
+            raise IOError(f"Не удалось сохранить файл конфигурации: {e}")
 
 
 @pytest.fixture
 def driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--window-size=1920,1080")
-    options.page_load_strategy = 'eager'
+    config = ConfigReader("config.json")
 
-    service = Service(executable_path=ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
+    chrome_options = Options()
+
+    if config.get("window_size"):
+        chrome_options.add_argument(f"--window-size={config.get('window_size')}")
+    if config.get("disable_notifications"):
+        chrome_options.add_argument("--disable-notifications")
+    if config.get("ignore_certificate_errors"):
+        chrome_options.add_argument("--ignore-certificate-errors")
+    if config.get("headless"):
+        chrome_options.add_argument("--headless")
+
+    driver = webdriver.Chrome(options=chrome_options)
     yield driver
-
     driver.quit()
